f200a406ad3b765dd07332d14a608b70dc5579cb
mario attrovio <mario.attrovio@gmail.com>
2021-10-16 11:54:27 +0200

    lab1 comoleted

---
+++ lab02/es2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define FILEIN "mat.txt"
+#define DIM 10
+
+int **malloc2dR();
+
+malloc2dP(int***);
+
+void separa(int **mat, int nr, int nc);
+
+ int main(){
+    int *bianche, *nere;
+    int nr, nc;
+    int mat**;
+
+   FILE *fp_read;
+
+   fp_read = fopen(FILEIN, "r");
+
+   fscanf(fp_read, "%d %d", &nr, &nc);
+
+
+
+
+ }
